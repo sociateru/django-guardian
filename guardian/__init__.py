8935601de314c8344e32f582b9a278b630bfce15
grumbler <serge.travin@gmail.com>
2015-05-02 01:47:13 +0300

    bump version

--- guardian/__init__.py
+++ guardian/__init__.py
@@ -3,15 +3,15 @@ Implementation of per object permissions for Django 1.2 or later.
 """
 from __future__ import unicode_literals
 
-VERSION = (1, 2, 6, 'dev')
+VERSION = (1, 2, 6, 0, 1)
 
-__version__ = '.'.join((str(each) for each in VERSION[:4]))
+__version__ = '.'.join((str(each) for each in VERSION[:5]))
 
 def get_version():
     """
     Returns shorter version (digit parts only) as string.
     """
-    return '.'.join((str(each) for each in VERSION[:4]))
+    return '.'.join((str(each) for each in VERSION[:5]))
 
 
 default_app_config = 'guardian.apps.GuardianConfig'
